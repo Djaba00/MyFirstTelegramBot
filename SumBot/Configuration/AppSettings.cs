24e6a3e55575c117f545061c52b0347535ac7d33
Timofey <94359448+Djaba00@users.noreply.github.com>
2023-02-13 20:31:55 +0600

    Update AppSettings.cs

--- SumBot/Configuration/AppSettings.cs
+++ SumBot/Configuration/AppSettings.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 namespace SumBot.Configuration
 {
-	public class AppSettings
-	{
-        /// <summary>
+    public class AppSettings
+    {
+	/// <summary>
         /// Telegram Bot API
         /// </summary>
         public string BotToken { get; set; }
